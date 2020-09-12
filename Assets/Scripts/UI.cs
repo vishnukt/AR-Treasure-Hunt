a4d4f8136c4926c96541a941e3983d17daf286e6
vishnukt <kt.vishnu19@gmail.com>
2020-09-12 18:50:39 +0530

    Adding all Files

---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour
+{
+    public GameObject db;
+    public string scenename;
+
+    public void exit()
+    {
+        Application.Quit();
+    }
+    public void menu_enable()
+    {
+        db.SetActive(true);
+    }
+    public void menu_disable()
+    {
+        db.SetActive(false);
+    }
+    
+    public void open_scene()
+    {
+        Application.LoadLevel(scenename);
+    }
+}
---
+++ Assets/Scripts/back.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class back : MonoBehaviour
+{
+    public bool isQuiting;
+    public GameObject quitMessage;
+    public GameObject db;
+
+    // Start is called before the first frame update
+    /*void Start()
+    {
+
+    }*/
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isQuiting == true && db.active==false)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Space))
+            {
+                Application.Quit();
+            }
+        }
+        if ((Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Space))&& db.active == false)
+        {
+            quitMessage.SetActive(true);
+            isQuiting = true;
+            StartCoroutine(QuitingTimer());
+        }
+    }
+    IEnumerator QuitingTimer()
+    {
+        yield return new WaitForSeconds(2);
+        isQuiting = false;
+        quitMessage.SetActive(false);
+    }
+}
